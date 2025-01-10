fc7f91e2175fc8882c3eb3119b61c58a8b8b6bb1
abaspnr <117168519+abaspnr@users.noreply.github.com>
2025-01-10 10:00:58 +0300

    a

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -7,12 +7,12 @@ function WeatherDisplay({ weatherData }) {
     <div className="card text-center bg-dark text-white mt-5 mx-auto" style={{ maxWidth: '400px' }}>
       <div className="card-body">
         <h2 className="card-title">{name}</h2>
-        <p className="card-text">
-          <strong>Durum:</strong> {weather[0].description} <br />
-          <strong>Sıcaklık:</strong> {main.temp}°C <br />
-          <strong>Nem:</strong> {main.humidity}% <br />
-          <strong>Rüzgar:</strong> {wind.speed} m/s
-        </p>
+        <p className="card-text" style={{ color: 'red' }}>
+  <strong>Durum:</strong> {weather[0].description} <br />
+  <strong>Sıcaklık:</strong> {main.temp}°C <br />
+  <strong>Nem:</strong> {main.humidity}% <br />
+  <strong>Rüzgar:</strong> {wind.speed} m/s
+</p>
       </div>
     </div>
   );
